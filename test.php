325cfcfbed873aa63e676f102a3eb8052c7a8d1e
cl598 <55933747+cl598@users.noreply.github.com>
2020-09-18 12:32:53 -0400

    neater code

--- test.php
+++ test.php
@@ -4,33 +4,33 @@
 
 <?php
 
-	# Christian Le
-    # Professor Toegel
-    # IT 202 - 009
-    # 21 September 2020
+# Christian Le
+# Professor Toegel
+# IT 202 - 009
+# 21 September 2020
 
-	# Create an array of numbers
-    $nums = array(2,3,4,4,5,6,7,8,8);
+# Create an array of numbers
+	$nums = array(2,3,4,4,5,6,7,8,8);
     
-    # Initalize with any number from array (more than 1)
+# Initalize with any number from array (more than 1)
 	$num = 2;
 
-	# Create a loop to show each value
-    echo "LOOP OUTPUT<br>";
-    foreach($nums as $num){
-    	echo $num . " " ;
-    }
+# Create a loop to show each value
+	echo "LOOP OUTPUT<br>";
+	foreach($nums as $num){
+		echo $num . " " ;
+	}
    
-   # Break between the two outputs
-    echo "<br><br>";
+# Break between the two outputs
+	echo "<br><br>";
     
-    # Loop outputs only even numbers
-    echo "LOOP EVEN OUTPUT<br>";
-    foreach($nums as $num){
-    	if($num % 2 == 0){
-        	echo $num . " ";
-        }
-    }
+# Loop outputs only even numbers
+	echo "LOOP EVEN OUTPUT<br>";
+	foreach($nums as $num){
+		if($num % 2 == 0){
+			echo $num . " ";
+		}
+	}
 
 ?>
 
